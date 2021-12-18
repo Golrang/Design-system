811c30ff24362e380bd70927d262524165770881
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-12-18 15:10:46 +0330

    CHART: Fix subscribe react query

--- src/pages/home/containers/chart.tsx
+++ src/pages/home/containers/chart.tsx
@@ -2,24 +2,23 @@ import { useEffect, useState } from 'react'
 import { QueryObserver, useQueryClient } from 'react-query'
 
 export const Chart = () => {
-  const client = useQueryClient()
-  const observer = new QueryObserver(client, {
-    queryKey: 'LIST',
-  })
   const [state, setstate] = useState<any>(null)
 
+  const client = useQueryClient()
+
+  const observer = new QueryObserver(client, {
+    queryKey: 'LIST',
+    onSuccess: (data) => setstate(data),
+  })
+
   useEffect(() => {
-    const unsubscribe = observer.subscribe((res) => {
-      setstate(res)
-    })
-    return () => {
-      unsubscribe()
-    }
+    const unsubscribe = observer.subscribe()
+    return () => unsubscribe()
   }, [])
 
   return (
     <div className="w-full bg-red-400 rounded h-full flex flex-col items-center justify-center p-4">
-      {state?.data?.map((item: any) => (
+      {state?.map((item: any) => (
         <span key={item.id} className="text-white">
           {item.name}
         </span>
