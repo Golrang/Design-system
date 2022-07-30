bcf75503ca09eea64fc801449dace55f4c5f9de2
somaye safavi <safavi.somaye@golrang.com>
2022-07-30 11:28:29 +0430

    change component button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,10 +1,75 @@
-import { ComponentProps } from "react";
-export type TButton = ComponentProps<"button">;
+import { Button, Dropdown, Form, Menu, Tooltip } from "antd";
+import { ItemType } from "antd/lib/menu/hooks/useItems";
+import { MenuProps } from "rc-menu";
+import { ComponentProps, useState } from "react";
+import { useController } from "react-hook-form";
 
-export const Button = ({ children, ...rest }: TButton) => {
+type TButton<G> = Omit<
+  ComponentProps<"input">,
+  "size" | "ref" | "name" | "type"
+> & {
+  name: G;
+  size?: "small" | "middle" | "large";
+  type?: "primary" | "dashed" | "link" | "text";
+  titleTooltiP?: string;
+  icon?: React.ForwardRefExoticComponent<any>;
+  shape?: "circle" | "round" | "default";
+  htmlType?: "submit" | "reset" | "button";
+  isDropDownButton?: boolean;
+  drowpdownItems?: ItemType[] | undefined;
+  drowpdownOnClick?: MenuProps["onClick"] | undefined;
+};
+
+export const ButtonG = <G extends string>({
+  name,
+  size,
+  type,
+  children,
+  titleTooltiP,
+  icon,
+  shape,
+  htmlType,
+  isDropDownButton,
+  drowpdownItems,
+  drowpdownOnClick,
+  ...rest
+}: TButton<G>) => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController({ name });
+  const menu = <Menu onClick={drowpdownOnClick} items={drowpdownItems} />;
   return (
-    <button className="bg-indigo-500 p-4 rounded text-white" {...rest}>
-      {children}
-    </button>
+    <Form.Item name={name}>
+      {titleTooltiP ? (
+        <Tooltip title={titleTooltiP}>
+          <Button
+            {...{ ...field }}
+            type={type}
+            icon={icon}
+            shape={shape}
+            htmlType={htmlType}
+            {...rest}
+          >
+            {children}
+          </Button>
+        </Tooltip>
+      ) : isDropDownButton ? (
+        <Dropdown.Button overlay={menu}>Actions</Dropdown.Button>
+      ) : (
+        <Button
+          {...{ ...field }}
+          type={type}
+          icon={icon}
+          shape={shape}
+          htmlType={htmlType}
+          {...rest}
+        >
+          {children}
+        </Button>
+      )}
+
+      {error && <div className="text-red-500">{error.message}</div>}
+    </Form.Item>
   );
 };
