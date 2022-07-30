6a4de8815a4677f67ca71b6270732b7324c8eda7
somaye safavi <safavi.somaye@golrang.com>
2022-07-30 11:29:16 +0430

    test component and use components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,17 @@
+import { ButtonG } from "components/button/Button";
 import { FormG } from "components/form/Form";
 import { InputG } from "components/input/Input";
+import { RadioGroupG } from "components/radio/RadioGroup";
 import { useWatch } from "react-hook-form";
-
-type TFormProps = { name: string; user: string; age: number };
+import { SearchOutlined } from "@ant-design/icons";
+type TFormProps = {
+  name: string;
+  user: string;
+  age: number;
+  other: number;
+  button: string;
+  button_1: string;
+};
 
 type TInputProps = keyof TFormProps;
 
@@ -14,12 +23,63 @@ const App2 = () => {
 };
 
 export const App = () => {
+  const options = [
+    { value: 1, label: "somaye", disabled: true },
+    { value: 2, label: "somayetest" },
+    { value: 3, label: "somaye3" },
+    { value: 4, label: "somaye4" },
+  ];
+
+  const items = [
+    {
+      key: "1",
+      label: "1st item",
+    },
+    {
+      key: "2",
+      label: "2nd item",
+    },
+    {
+      key: "3",
+      label: "3rd item",
+    },
+  ];
+
   return (
     <div className=" max-w-md mx-auto p-32">
       <FormG<TFormProps> onSubmit={(state) => console.log(state.user)}>
         <App2 />
         <InputG<TInputProps> name="user" label="User" />
-        <button type="submit">Submit</button>
+        <RadioGroupG<TInputProps>
+          options={options}
+          name="age"
+          optionType="button"
+          size="large"
+        />
+        <RadioGroupG<TInputProps> options={options} name="other" />
+        <ButtonG<TInputProps>
+          name="button"
+          htmlType="reset"
+          type="primary"
+          icon={<SearchOutlined />}
+          isDropDownButton={true}
+          drowpdownItems={items}
+        >
+          clickme
+        </ButtonG>
+
+
+        <ButtonG<TInputProps>
+          name="button_1"
+          htmlType="button"
+          type="primary"
+          danger
+          // loading
+        >
+          clickme
+        </ButtonG>
+
+        {/* <button type="submit">Submit</button> */}
       </FormG>
     </div>
   );
