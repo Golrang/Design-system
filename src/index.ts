22e7d7ffefe31c454f706d1521df429d5153393c
amirHosseinKhademii <sourena2575@gmail.com>
2022-07-30 13:20:59 +0200

    Update the export file

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ export { Form } from "./components/form/Form";
 export { Input } from "./components/input/Input";
 export { Select } from "./components/select/Select";
 export { CheckBox } from "./components/checkbox/CheckBox";
-export { TextArea } from "./components/textarea/TextArea";
+export { TextArea } from "./components/text-area/TextArea";
