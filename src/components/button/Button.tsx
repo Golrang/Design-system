ae6d4f8d693455e647711201049d71ae3c75e751
somaye safavi <safavi.somaye@golrang.com>
2022-08-14 14:54:47 +0430

    dommy button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,13 +1,15 @@
-import { Button as AntButton } from "antd";
-import { TFormButton } from "components/form-button/FormButton";
+import { Button as AntButton, ButtonProps } from "antd";
+// import { TFormButton } from "components/form-button/FormButton";
 import { forwardRef } from "react";
 
+export type TButton = {
+  className?: string;
+} & Partial<ButtonProps>;
+
 export const Button = forwardRef(
-  ({ children, ...rest }: TFormButton, ref?: any) => (
-    <>
-      <AntButton {...rest} {...{ ref }}>
-        {children}
-      </AntButton>
-    </>
+  ({ children, ...rest }: TButton, ref?: any) => (
+    <AntButton {...rest} {...{ ref }}>
+      {children}
+    </AntButton>
   )
 );
