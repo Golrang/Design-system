4650356d6a2d737b784c820e499025c21acb8984
amirHosseinKhademii <sourena2575@gmail.com>
2022-08-14 12:43:01 +0200

    Create use message hook

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import "./styles/index.css";
 import "antd/dist/antd.css";
-//export { Button } from "./components/button/Button";
+
 export { Form } from "./components/form/Form";
 export { FormInput } from "./components/form-input/FormInput";
 export { FormSelect } from "./components/form-select/FormSelect";
 export { CheckBox } from "./components/checkbox/CheckBox";
-export { TextArea } from "./components/form-textarea/FormTextArea";
+export { FormTextArea } from "./components/form-textarea/FormTextArea";
 export { Radio } from "./components/radio/Radio";
 export { Uploader } from "./components/uploader";
 export { FormUploader } from "./components/form-uploader";
 export * from "./icons";
+export * from "./hooks";
