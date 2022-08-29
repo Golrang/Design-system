84331307ba7ec5ae12b05e3183f057e73d8ad295
Feri <ranaei.farshad@golrang.com>
2022-08-29 12:07:41 +0430

    Add DatePicker to DS

---
+++ src/components/datepicker/DatePicker.tsx
@@ -0,0 +1,9 @@
+import { DatePicker as AntDatePicker } from "antd";
+import { forwardRef } from "react";
+import { TDatePicker } from "./datepicker.types";
+
+export const DatePicker = forwardRef(({ ...rest }: TDatePicker, ref: any) => {
+    return (
+        <AntDatePicker {...rest} ref={ref} />
+    )
+})
